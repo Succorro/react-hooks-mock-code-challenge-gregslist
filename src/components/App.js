3024a85d07d651ca618fedb0d789860c8a417a6d
Steven Gutierrez <stevogbmv@gmail.com>
2023-02-24 16:26:50 -0800

    commit tree 53a538b886c51c2ba68d9d2c7cbec2daf0f94037

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React,{ useEffect, useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+  const [search, setSearch] = useState('')
+
+  useEffect(()=> {
+    fetch('http://localhost:6001/listings')
+    .then(r=> r.json())
+    .then(data => setListings(data))
+  },[])
+
+  function handleDelete(id){
+    const newListings = listings.filter((listing) => listing.id !== id)
+    setListings(newListings)
+  };
+
+  const displayedListings = listings.filter((listing) => listing.description.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={setSearch}/>
+      <ListingsContainer 
+      listings={displayedListings}
+      onDelete={handleDelete}
+      />
     </div>
   );
 }
